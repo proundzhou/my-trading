0205e3fd35976d29a1b7ba45739e7ea1d7964ddb
proundzhou <proundzhou@163.com>
2019-11-15 20:22:20 +0800

    去除测试用OrderServiceImpl

--- refund-after-sele/src/main/java/com/zhou/refundaftersele/service/impl/IOrderServiceImpl.java
+++ refund-after-sele/src/main/java/com/zhou/refundaftersele/service/impl/IOrderServiceImpl.java
@@ -4,9 +4,8 @@ import com.zhou.refundaftersele.domain.dto.OrderMapDto;
 import com.zhou.refundaftersele.service.IOrderService;
 import org.springframework.stereotype.Service;
 
-@Service
-public class IOrderServiceImpl implements IOrderService {
-    @Override
+public class IOrderServiceImpl {
+
     public Double getSpend(OrderMapDto orderMapDto) {
         return 123.1;
     }
