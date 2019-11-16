1389c10df7f0819f7e9a79e27d634fa1a400fdcd
wenmou-yyy <2272154410@qq.com>
2019-11-16 11:27:16 +0800

    评论功能  nacos部署  （不带文件上传）

--- evaluate/src/main/java/com/wqh/project/evaluate/EvaluateApplication.java
+++ evaluate/src/main/java/com/wqh/project/evaluate/EvaluateApplication.java
@@ -3,8 +3,10 @@ package com.wqh.project.evaluate;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 @MapperScan("com.wqh.project.evaluate.mapper")
 public class EvaluateApplication {
 
